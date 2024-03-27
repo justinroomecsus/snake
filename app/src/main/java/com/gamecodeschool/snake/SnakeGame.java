7f7fc339528ceaddebfc52076e207aedfd80ec29
justinroomecsus <90804852+justinroomecsus@users.noreply.github.com>
2024-03-26 18:37:02 -0700

    Adding pause button functionality

--- app/src/main/java/com/gamecodeschool/snake/SnakeGame.java
+++ app/src/main/java/com/gamecodeschool/snake/SnakeGame.java
@@ -1,12 +1,15 @@
 package com.gamecodeschool.snake;
-
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.Typeface;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
@@ -17,10 +20,16 @@ import android.view.SurfaceView;
 
 import java.io.IOException;
 
+@SuppressLint("ViewConstructor")
 public class SnakeGame extends SurfaceView implements Runnable, GameObject {
     private static final int NUM_BLOCKS_WIDE = 40;
     private static final int TARGET_FPS = 10;
     private static final long MILLIS_PER_SECOND = 1000;
+
+    private Context mContext;
+    private Bitmap backgroundBitmap;
+    private Bitmap mPauseButtonBitmap;
+    private Bitmap munPauseButtonBitmap;
 
     private Thread mThread = null;
     private long mNextFrameTime;
@@ -36,6 +45,9 @@ public class SnakeGame extends SurfaceView implements Runnable, GameObject {
     private Paint mPaint;
     private Snake mSnake;
     private Apple mApple;
+    private boolean mGameStarted = false;
+private volatile boolean mGameJustStarted = false;
+
 
     public SnakeGame(Context context, Point size) {
         super(context);
@@ -50,6 +62,11 @@ public class SnakeGame extends SurfaceView implements Runnable, GameObject {
         mPaint = new Paint();
         mApple = new Apple(context, new Point(NUM_BLOCKS_WIDE, mNumBlocksHigh), blockSize);
         mSnake = new Snake(context, new Point(NUM_BLOCKS_WIDE, mNumBlocksHigh), blockSize);
+        mPauseButtonBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.pause_button);
+        mPauseButtonBitmap = Bitmap.createScaledBitmap(mPauseButtonBitmap, 100, 100, false);
+        munPauseButtonBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.unpause_button);
+        munPauseButtonBitmap = Bitmap.createScaledBitmap(munPauseButtonBitmap, 100, 100, false);
+
     }
 
     private void initializeSoundPool(Context context) {
@@ -89,6 +106,8 @@ public class SnakeGame extends SurfaceView implements Runnable, GameObject {
         mApple.spawn();
         mScore = 0;
         mNextFrameTime = System.currentTimeMillis();
+            mGameJustStarted = true;
+
     }
 
     @Override
@@ -122,23 +141,49 @@ public class SnakeGame extends SurfaceView implements Runnable, GameObject {
         if (mSnake.detectDeath()) {
             mSP.play(mCrashID, 1, 1, 0, 0, 1);
             mPaused = true;
+            mGameStarted = false;
         }
     }
 
     public void draw() {
         if (mSurfaceHolder.getSurface().isValid()) {
             mCanvas = mSurfaceHolder.lockCanvas();
-            mCanvas.drawColor(Color.argb(255, 26, 128, 182));
-            mPaint.setColor(Color.argb(255, 255, 255, 255));
-            mPaint.setTextSize(120);
-            mCanvas.drawText("" + mScore, 20, 120, mPaint);
+            // background adding code
+            if (backgroundBitmap == null) {
+                backgroundBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.background_image1);
+            }
+            int canvasWidth = mCanvas.getWidth();
+            int canvasHeight = mCanvas.getHeight();
+            Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.background_image1);
+            Bitmap scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, canvasWidth, canvasHeight, true);
+            mCanvas.drawBitmap(scaledBitmap, 0, 0, null);
+
+            int x = mCanvas.getWidth() - mPauseButtonBitmap.getWidth() - 20;
+            int y = 20;
+            if (mPaused) {
+                mCanvas.drawBitmap(munPauseButtonBitmap, x, y, null);
+            } else {
+                mCanvas.drawBitmap(mPauseButtonBitmap, x, y, null);
+            }
+
+            Typeface typeface = Typeface.createFromAsset(getContext().getAssets(), "myfont.ttf");
+            mPaint.setTypeface(typeface);
+
+            mPaint.setColor(Color.argb(255, 220, 20, 60));
+            mPaint.setFakeBoldText(true);
+            mPaint.setTextSize(100);
+            mCanvas.drawText("  " + mScore, 10, 100, mPaint);
+            mPaint.setTextSize(40);
+            mPaint.setColor(Color.argb(255, 0, 0, 0));
+            mCanvas.drawText("Justin", 2000, 950, mPaint);
+            mCanvas.drawText("Muhammad", 2000, 1000, mPaint);
+            mPaint.setFakeBoldText(false);
             mApple.draw(mCanvas, mPaint);
             mSnake.draw(mCanvas, mPaint);
-
-            if (mPaused) {
-                mPaint.setColor(Color.argb(255, 255, 255, 255));
-                mPaint.setTextSize(250);
-                mCanvas.drawText(getResources().getString(R.string.tap_to_play), 200, 700, mPaint);
+            if (mPaused && !mGameStarted) {
+                mPaint.setColor(Color.argb(255, 0, 0, 0));
+                mPaint.setTextSize(100);
+                mCanvas.drawText(getResources().getString(R.string.Touch_to_play), 600, 925, mPaint);
             }
             mSurfaceHolder.unlockCanvasAndPost(mCanvas);
         }
@@ -146,19 +191,33 @@ public class SnakeGame extends SurfaceView implements Runnable, GameObject {
 
     @Override
     public boolean onTouchEvent(MotionEvent motionEvent) {
+        if (mPaused && !mGameStarted) {
+        mGameStarted = true;
+
+        if (mGameJustStarted) {
+                    mGameJustStarted = false;
+                }
+    }
+        int x = (int) motionEvent.getX();
+        int y = (int) motionEvent.getY();
+        int canvasWidth = getWidth();
+        int canvasHeight = getHeight();
+        int pauseButtonX = canvasWidth - mPauseButtonBitmap.getWidth() - 20;
+        int pauseButtonY = 20;
+        int pauseButtonWidth = mPauseButtonBitmap.getWidth();
+        int pauseButtonHeight = mPauseButtonBitmap.getHeight();
+
         switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_UP:
-                if (mPaused) {
+                if (x >= pauseButtonX && x <= (pauseButtonX + pauseButtonWidth) && y >= pauseButtonY && y <= (pauseButtonY + pauseButtonHeight)) {
+                    mPaused = !mPaused;
+                } else if (!mGameJustStarted || !mPlaying) {
                     mPaused = false;
                     newGame();
-                    return true;
+                } else if (!mPaused){
+                    mSnake.switchHeading(motionEvent);
                 }
-
-                mSnake.switchHeading(motionEvent);
-                break;
-
-            default:
-                break;
+                return true;
         }
         return true;
     }
